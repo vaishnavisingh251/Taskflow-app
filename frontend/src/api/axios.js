cc47cc9a9e4175ba8f0809c9833b269208f8eaf8
Vaishnavi Singh <vaishsingh9696@gmail.com>
2026-05-02 12:52:25 +0530

    connected frontend to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'taskflow-app-production-bf6b.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
